967524f647fccbdaaf3cebc0ce752adf0bc75efd
Serena Ngo <sngo@csumb.edu>
2026-03-17 23:26:15 -0700

    completed HW2

--- HW/hw2/js/quiz_script.js
+++ HW/hw2/js/quiz_script.js
@@ -1,10 +1,14 @@
-//TODO: Finish adding listeners to user clicks
-//TODO: Validate submite
-//TODO: Feedback (correct/incorrect) with images
+//TODO: Score not appearing
 //TODO: Bootstrap
+
 // Event Listeners
-document.querySelector("#submitBtn").addEventListener("click", checkAnswer);
-// Global Variables
+document.querySelector("#submitBtn").addEventListener("click", submitQuiz);
+document.querySelector("#resetBtn").addEventListener("click", initializeGame);
+
+// Global Variableslet score = 0;
+let attempts = 0;
+let userAnswer = {} //create a dictionary for user answers - will be compared to dict question answers
+
 // Dictionary holding question types - 5 question types
 const quizTypes = {
     1: "multiple_choice",
@@ -19,14 +23,14 @@ const questions = [
         type: 1,
         id: "q1",
         question: "Which ocean is on the west side of the US?",
-        choices: ["Atlantic_Ocean", "Pacific_Ocean", "Artic_Ocean", "Indian_Ocean"],
+        choices: ["Atlantic_Ocean", "Pacific_Ocean", "Arctic_Ocean", "Indian_Ocean"],
         answer: "Pacific Ocean"
     },
     {
         type: 1,
         id: "q2",
         question: "What is the name of the largest swamp?",
-        choices: ["Atchafalaya_Basin", "Pacific_Ocean", "Arctic_Ocean", "Indian_Ocean"],
+        choices: ["Atchafalaya_Basin", "Okefenokee_Swamp", "Great_Basin", "Great_Dismal_Swamp"],
         answer: "Atchafalaya Basin"
     },
     {
@@ -54,15 +58,16 @@ const questions = [
         type: 3,
         id: "q6",
         question: "Which bridge is in California?",
-        choices: ["images/wiki_GoldenGateBridge.jpg", "images/wiki_VerrazanoBridge.jpg"],
-        answer: "mages/wiki_GoldenGateBridge.jpg"
+        choices: [{srcKey: "images/wiki_GoldenGateBridge.jpg", value: "goldenGate"},
+            {srcKey: "images/wiki_VerrazanoBridge.jpg", value: "VerrazanoBridge"}],
+        answer: "goldenGate"
     },
     {
         type: 3,
         id: "q7",
         question: "Which geyser is from Yellow Stone National Park in Wyoming?",
-        choices: ["images/wiki_GoldenGateBridge.jpg", "images/wiki_VerrazanoBridge.jpg"],
-        answer: "Missouri River"
+        choices: [{srcKey: "images/wiki_GrandPrismaticSpring.jpg", value: "GPSpring"}, {srcKey: "images/wiki_FlyGeyser.jpg", value: "flyGeyser"}],
+        answer: "GPSpring"
     },
     {
         type: 4,
@@ -85,20 +90,135 @@ const questions = [
     },
 ]
 
-let score = 0;
-const userAnswer = {}
+// Multiple Choice Questions
+function displayMultipleChoiceQuestion(pos, id, question_string, choices) {
+    const root_div = document.querySelector(`#${id}`);
+    root_div.innerHTML = ""; //Clear choices
+    root_div.innerHTML = `
+        <h2 class="question_num">Question ${pos+1}</h2>
+        <h1 class="question_text">${question_string}</h1>
+        <div id="${id}_feedback" class="question-feedback"></div>
+        <div class="row" id="${id}_choices"></div>
+    `;
 
-// Multiple choice questions
-function displayMultipleChoiceQuestion(index, id, question_string, choices) {
-    root_div = document.querySelector(`#${id}`);
-    root_div.innerHTML += `<h2>Question ${index+1}</h2>`;
-    root_div.innerHTML += `<h1>${question_string}</h1>`;
+    const choicesContainer = document.querySelector(`#${id}_choices`);
 
-    // mc choices
+    //Create choices in a 2x2 row
     for (let i = 0; i < choices.length; i++) {
         let mcChoice = choices[i];
-        root_div.innerHTML += `<input type="radio" name="${id}" id="${id}_${mcChoice}"
-        value="${mcChoice}"> <label for="${id}_${mcChoice}"> ${mcChoice.replaceAll("_", " ")}</label>`;
+        choicesContainer.innerHTML += `
+            <div class="col-6 mb-3">
+                <label class="choice-box d-flex align-items-center justify-content-center">
+                    <input type="radio" name="${id}" value="${mcChoice}">
+                    <span class="choice-text">${mcChoice.replaceAll("_", " ")}</span>
+                </label>
+            </div>
+        `;
+    }
+
+    // Event listener for selection highlight
+    const boxes = choicesContainer.querySelectorAll('input[type="radio"]');
+    boxes.forEach(box => {
+        box.addEventListener('change', (e) => {
+            // Remove active class from all boxes in this question
+            boxes.forEach(b => b.parentElement.classList.remove('selected'));
+            // Add select color to the clicked box
+            e.target.parentElement.classList.add('selected');
+            userAnswer[id] = e.target.value.replaceAll("_", " "); //Change dictionary names to display names
+            console.log('User chose:', userAnswer[id]);
+        });
+    });
+}
+
+// Dropdown questions
+function displayDropDownQuestion(pos, id, question_string, choices) {
+    root_div = document.querySelector(`#${id}`);
+    root_div.innerHTML = ""; //Clear choices
+    root_div.innerHTML += `<h2 class="question_num">Question ${pos+1}</h2>`;
+    root_div.innerHTML += `<h1 class="question_text">${question_string}</h1>`;
+
+    // Feedback
+    root_div.innerHTML += `<div id="${id}_feedback" class="question-feedback"></div>`;
+
+    //Dropdown Choices
+    let str = `<select id="${id}_select"><option value="">Select One</option>`
+    for (let i = 0; i < choices.length; i++) {
+        let ddChoice = choices[i];
+        str += `<option value="${ddChoice}">${ddChoice.replaceAll("_", " ")}</option>`
+    }
+    str += `</select>`
+    root_div.innerHTML += str;
+
+     //Event Listener to User Clicks
+    document.querySelector(`#${id}_select`).addEventListener("change", (event) => {
+        if (event.target.value !== "") {
+            userAnswer[id] = event.target.value.replaceAll("_", " ");
+            console.log('User chose: ' + userAnswer[id]);
+        }
+    });
+}
+
+// Image Select questions
+function displayImageSelectQuestion(pos, id, question_string, choices) {
+    root_div = document.querySelector(`#${id}`);
+    root_div.innerHTML = ""; //Clear choices
+    root_div.innerHTML += `<h2 class="question_num">Question ${pos+1}</h2>`;
+    root_div.innerHTML += `<h1 class="question_text">${question_string}</h1>`;
+    //Feedback
+    root_div.innerHTML += `<div id="${id}_feedback" class="question-feedback"></div>`;
+
+    //image choices
+    for (let i = 0; i < choices.length; i++) {
+        let imgChoice = choices[i];
+        root_div.innerHTML += `<img class="img_container" id="${id}_${imgChoice.value}" src="${imgChoice.srcKey}" width="200" height="200">`
+    }
+
+    //Event Listener to User Clicks
+    for (let i = 0; i < choices.length; i++) {
+        let imgChoice = choices[i];
+        document.querySelector(`#${id}_${imgChoice.value}`).addEventListener("click", (event) => {
+            userAnswer[id] = event.target.id.replaceAll(`${id}_`, "");
+            choices.forEach(choice => {
+                document.querySelector(`#${id}_${choice.value}`).style.border = "none"; //reset image without border
+            });
+            // Add green border for clicked image
+            event.target.style.border = "5px solid #E6D154";
+            console.log('User chose: ' + userAnswer[id]);
+        });
+    }
+}
+
+// Input questions
+function displayInputQuestion(pos, id, question_string) {
+    root_div = document.querySelector(`#${id}`);
+    root_div.innerHTML = ""; //Clear choices
+    root_div.innerHTML += `<h2 class="question_num">Question ${pos+1}</h2>`;
+    root_div.innerHTML += `<h1 class="question_text">${question_string}</h1>`;
+    // Feedback
+    root_div.innerHTML += `<div id="${id}_feedback" class="question-feedback"></div>`;
+    root_div.innerHTML += `<input type="text" id="playerInput_${id}" class="form-control input_form">`;
+
+    // Listen to user's click
+    document.querySelector(`#playerInput_${id}`).addEventListener("blur", (event) => {
+        userAnswer[id] = event.target.value;
+    });
+}
+
+// Multiple answer questions
+function displayMultipleAnswerQuestion(pos, id, question_string, choices) {
+    root_div = document.querySelector(`#${id}`);
+    root_div.innerHTML = ""; //Clear choices
+    root_div.innerHTML += `<h2 class="question_num">Question ${pos+1}</h2>`;
+    root_div.innerHTML += `<h1 class="question_text">${question_string}</h1>`;
+
+    // Feedback
+    root_div.innerHTML += `<div id="${id}_feedback" class="question-feedback"></div>`;
+
+    // mc choices -- og
+    for (let i = 0; i < choices.length; i++) {
+        let maChoice = choices[i];
+        root_div.innerHTML += `<input type="checkbox" id="${id}_${maChoice}">
+        <label for="${id}_${maChoice}" class="subtitle">${maChoice}</label>`
     }
 
     //Listen to user's click
@@ -106,68 +226,34 @@ function displayMultipleChoiceQuestion(index, id, question_string, choices) {
         let mcChoice = choices[i];
         document.querySelector(`#${id}_${mcChoice}`).addEventListener("change", (event) => {
             if (event.target.checked) {
-                userAnswer[id] = event.target.value;
-                console.log('User chose: ' + event.target.value);
+                if (userAnswer[id] == null) { //if question has not been answered yet, initialize answer array
+                    userAnswer[id] = [];
+                }
+                userAnswer[id].push(event.target.id.replaceAll(`${id}_`, "")); //push selected choices into array
+                console.log('User chose: ' + userAnswer[id]);
+            } else { //update array if element is unchecked
+                let valueToRemove = event.target.id.replaceAll(`${id}_`, "")
+                updatedArray = userAnswer[id].filter(item => item !== valueToRemove); //keep every item that is not the item to remove
+                userAnswer[id] = updatedArray; //Update the array
+                console.log('User chose: ' + userAnswer[id]);
             }
         });
     }
 }
 
-// Dropdown questions
-function displayDropDownQuestion(index, id, question_string, choices) {
-    root_div = document.querySelector(`#${id}`);
-    root_div.innerHTML += `<h2>Question ${index+1}</h2>`;
-    root_div.innerHTML += `<h1>${question_string}</h1>`;
-    //dropdown choices
-    let str = `<select id="${id}"><option value="">Select One</option>`
-    for (let i = 0; i < choices.length; i++) {
-        let ddChoice = choices[i];
-        str += `<option value="${ddChoice}">${ddChoice}</option>`
-    }
-    str += `</select>`
-    root_div.innerHTML += str;
-}
-
-// Image Select questions
-function displayImageSelectQuestion(index, id, question_string, choices) {
-    root_div = document.querySelector(`#${id}`);
-    root_div.innerHTML += `<h2>Question ${index+1}</h2>`;
-    root_div.innerHTML += `<h1>${question_string}</h1>`;
-    //image choices
-    for (let i = 0; i < choices.length; i++) {
-        let imgChoice = choices[i];
-        root_div.innerHTML += `<img src="${imgChoice}" width="100" height="100">`
-    }
-}
-
-// Input questions
-function displayInputQuestion(index, id, question_string) {
-    root_div = document.querySelector(`#${id}`);
-    root_div.innerHTML += `<h2>Question ${index+1}</h2>`;
-    root_div.innerHTML += `<h1>${question_string}</h1>`;
-    root_div.innerHTML += `<input type="text" id="playerInput_${id}">`
-}
-
-// Multiple choice questions
-function displayMultipleAnswerQuestion(index, id, question_string, choices) {
-    root_div = document.querySelector(`#${id}`);
-    root_div.innerHTML += `<h2>Question ${index+1}</h2>`;
-    root_div.innerHTML += `<h1>${question_string}</h1>`;
-
-    // mc choices
-    for (let i = 0; i < choices.length; i++) {
-        let maChoice = choices[i];
-        root_div.innerHTML += `<input type="checkbox" id="${id}_${maChoice}">
-        <label for="${id}_${maChoice}">${maChoice}</label>`
-    }
-}
-
 function initializeGame() {
-    // First question
+    
+    // Hide Reset Button
+    document.querySelector("#resetBtn").classList.add("d-none");
+    // Show Submit Button
+    document.querySelector("#submitBtn").classList.remove("d-none");
+    //Reset user answer
+    userAnswer = {};
+
     for (let i = 0; i < questions.length; i++) {
         let question = questions[i];
         if (question.type == 1) {
-            displayMultipleChoiceQuestion(i, question.id, question.question, question.choices);
+            displayMultipleChoiceQuestion(i, question.id, question.question, shuffleArray(question.choices));
         }
         if (question.type == 2) {
             displayDropDownQuestion(i, question.id, question.question, question.choices);
@@ -179,18 +265,112 @@ function initializeGame() {
             displayInputQuestion(i, question.id, question.question);
         }
         if (question.type == 5) {
-            displayMultipleAnswerQuestion(i, question.id, question.question, question.choices);
+            displayMultipleAnswerQuestion(i, question.id, question.question, shuffleArray(question.choices));
         }
     }
 }
 
-function checkAnswer() {
-    console.log(userAnswer);
+//Validate quiz
+function isQuizValid(userAnswer) {
+    let isValid = true;
+
+    for (let i=1; i < questions.length+1; i++) { //check if all questions have been completed
+        let qID = `q${i}`;
+        let feedback = document.querySelector(`#${qID}_feedback`);
+        feedback.textContent = ""; //Clear old feedback
+
+        let checkAnswer = userAnswer[qID];
+        console.log(checkAnswer);
+        if(checkAnswer == null) {
+            feedback.textContent = "Please provide an answer."
+            feedback.style.color = "#E6D154";
+            isValid = false;
+        }
+    }
+    return isValid;
+}
+
+function checkAnswer(pos, id) {
+    console.log(userAnswer[id] == questions[pos].answer);
+    let feedback = document.querySelector(`#${id}_feedback`);
+    let correctAnswer = questions[pos].answer;
+    let isCorrect = false;
+
+    // For checking multiple answers questions
+    if (Array.isArray(questions[pos].answer)) {
+        let user_set = new Set(userAnswer[id])
+        let answer_set = new Set(questions[pos].answer)
+        isCorrect = 
+            user_set.size == answer_set.size && user_set.intersection(answer_set).size == answer_set.size;
+    } else { 
+        isCorrect = userAnswer[id]?.toLowerCase() == correctAnswer.toLowerCase();
+    }
+
+    //For correct answers - display text in green and add +10
+    if (isCorrect) {
+        feedback.textContent = "Correct answer!";
+        feedback.style.color = "#7FBA6D";
+        score += 10;
+    } else { //For incorrect answers - display text in red
+        feedback.textContent = `Incorrect answer! Correct answer: ${correctAnswer}`;
+        feedback.style.color = "#FF1111"
+    }
+}
+
+function shuffleArray(choices) {
+    const arrayCopy = [...choices];
+    for (let i=0; i<5; i++) {
+        let randomPos = Math.floor(Math.random() * choices.length);
+        let randomPos2 = Math.floor(Math.random() * choices.length);
+
+        console.log(randomPos);
+        console.log(randomPos2);
+
+        let x = arrayCopy[randomPos2];
+        let temp = arrayCopy[randomPos]
+        arrayCopy[randomPos] = x;
+        arrayCopy[randomPos2] = temp;
+    }
+    console.log(arrayCopy);
+    return arrayCopy;
+}
+
+function submitQuiz() {
+    score = 0; //reset score
+    
+    //Check if all questions have been answered
+    if (!isQuizValid(userAnswer)) {
+        return;
+    }
+
+    //If all questions have been answered, grade the quiz
+    for (let i = 0; i < questions.length; i++) {
+        let id = questions[i].id;
+        checkAnswer(i, id);
+    }
+
+    //Calculate score
+    const totalScore = document.querySelector("#score");
+    totalScore.textContent = `${score}`;
+    //Show certain score color depending on the total score
+    totalScore.style.color = score >= 80 ? "#7FBA6D" : "#FF1111";
+    if (score >= 80) {
+        totalScore.textContent = `${score} - Congratulations!`;
+    }
+
+    console.log("Final Score:", score);
+    //Count attempt
+    attempts++;
+    const totalAttempts = document.querySelector("#attempts");
+    totalAttempts.textContent = `${attempts}`;
+    gameOver();
 }
 
 function gameOver() {
+    submitBtn = document.querySelector("#submitBtn");
     resetBtn = document.querySelector("#resetBtn");
-    resetBtn.style.display = "inline"; // shows reset button
+    submitBtn.classList.add("d-none"); // Hide guess button
+    resetBtn.classList.remove("d-none"); // Show reset button
 }
 
 initializeGame();
