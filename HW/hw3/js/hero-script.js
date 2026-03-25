6688ca79c6ac19cbede0bd9a16ea55d6db9f352d
Serena Ngo <sngo@csumb.edu>
2026-03-24 22:55:59 -0700

    completed base assignment for hw 3

---
+++ HW/hw3/js/hero-script.js
@@ -0,0 +1,73 @@
+const urlParams = new URLSearchParams(window.location.search);
+const hero_name = urlParams.get('hero');
+
+function getHeroRender(heroName) {
+    const formatted = heroName
+        .replaceAll(" ", "_")
+        .replaceAll("&", "%26");
+
+    return `https://deadlock.wiki/Special:Redirect/file/${formatted}_Render.png`;
+}
+
+async function displayHeroPage() {
+
+    // API fetch
+    let url = `https://assets.deadlock-api.com/v2/heroes/by-name/${hero_name}`;
+    let response = await fetch(url);
+    let hero = await response.json();
+    // const heroIntro = document.querySelector(`#hero-header`); // Grab the hero-container id from HTML
+    console.log(hero);
+    
+    document.querySelector("#hero-image-icon").innerHTML = `<img src="${hero.images.icon_image_small} ">`;
+    document.querySelector("#hero-name").innerHTML = hero_name.toUpperCase();
+
+    console.log(hero.description.role != null);
+    if (hero.description.role != null) {
+        document.querySelector("#hero-role").innerHTML = hero.description.role;
+    } else {
+        document.querySelector("#hero-role").innerHTML = " ";
+    }
+    if (hero.description.playstyle != null) {
+        document.querySelector("#hero-playstyle").innerHTML = hero.description.playstyle;
+    } else {
+        document.querySelector("#hero-playstyle").innerHTML = " ";
+    }
+    if (hero.description.role == null && hero.description.playstyle == null) {
+        document.querySelector("#hero-hideout").innerHTML = hero.hero_stats_ui.hideout_rich_presence;
+    }
+
+    // Playstyle and gunplay tags
+    if (hero.gun_tag != null) {
+        let gun_tag = hero.gun_tag.toLowerCase();
+        document.querySelector("#hero-gun-tag").innerHTML = gun_tag;
+    } else {
+        document.querySelector("#hero-gun-tag").innerHTML = "-";
+    }
+    if (hero.hero_type != null) {
+        document.querySelector("#hero-type-tag").innerHTML = hero.hero_type;        
+    } else {
+        document.querySelector("#hero-type-tag").innerHTML = "-";
+    }
+
+    // Introduction and Lore
+    document.querySelector("#lore-text").innerHTML = hero.description.lore;
+    
+    //Loop for tags
+    const container = document.querySelector("#hero-desc-tag");
+    hero.tags.forEach(tag => {
+        const span = document.createElement("span");
+        span.classList.add("hero-tag");
+        span.textContent = tag;
+        container.appendChild(span);
+    });
+    
+    // Hero Art
+    let image = getHeroRender(hero.name);
+    document.querySelector("#hero-art").innerHTML = `<img src="${image}">`;
+
+    hero_id = hero.id;
+    console.log(hero.id);
+    // displayHeroAbilities(hero_id);
+}
+
+displayHeroPage();
--- HW/hw3/js/script.js
+++ HW/hw3/js/script.js
@@ -1,7 +1,95 @@
-// TODO: At least one form element for user input
-// At least one validation for Form Element
-// At least one event listener
-// At least one fetch call
-// Data is retrieved from existing API
-// API data displayed in a user-friendly format
-// Bootstrap/CSS
+// Event Listeners
+document.querySelector("#filterHeroes").addEventListener("input", filterHeroes);
+
+// Global Variables
+let hero_array;
+
+// Populate hero page with hero card image and name
+async function displayHeroes() {
+    //pull data
+    const hero_div = document.querySelector(`#hero-list`); // Grab the hero-container id from HTML
+    //empty array
+    hero_div.innerHTML = "";
+
+    // Apped html elements into index
+    for (let i=0; i < hero_array.length; i++) {
+
+        let hero_name = hero_array[i].name;
+        let hero_class_name = hero_array[i].class_name;
+        let hero_portrait = hero_array[i].images.icon_hero_card;
+        hero_div.innerHTML += `
+            <div id="${hero_class_name}_card" class="hero-container">
+                <div class="hero-name" class="selectHeroes">${hero_name}</div>
+                <div class="hero-items" class="selectHeroes"><img src=${hero_portrait} alt="${hero_name}"></div>
+            </div>
+        `;
+    }
+    
+    // Event Listener for each hero card
+    for (let i = 0; i < hero_array.length; i++) {
+        let hero_name = hero_array[i].name;
+        let hero_class_name = hero_array[i].class_name;
+
+         // When each hero card is clicked, it will take the user to the hero page
+        document.querySelector(`#${hero_class_name}_card`).addEventListener("click", (event) => {
+            hero_array.forEach(hero => {
+                // pass in hero_name variable into html
+                window.location = `hero.html?hero=${encodeURIComponent(hero_name)}`;
+            });
+        });
+
+        // Event Listener for hover
+        document.querySelector(`#${hero_class_name}_card`).addEventListener("mouseover", function(event) {
+            event.target.style.transform = "scale(1.1)";
+            event.target.style.transition = "transform 0.3s ease";
+        });
+        document.querySelector(`#${hero_class_name}_card`).addEventListener("mouseout", function(event) {
+            event.target.style.transform = "scale(1)";
+        });
+    }
+}
+
+
+// Function for searching for heroes
+async function filterHeroes(event) {
+    await download_data();
+    let filter_value = event.target.value.toLowerCase();
+    let hero_div = document.querySelector("#hero-list");
+    
+
+    let filtered_hero = hero_array.filter((hero) => hero.name.toLowerCase().startsWith(filter_value.toLowerCase()));
+
+    //If no results
+    if (filtered_hero.length == 0 && filter_value != "") {
+        hero_div.innerHTML = `
+            <div class="error-title">Hero not found.</div>
+        `;
+        return;
+    }
+
+    //Otherwise, display results
+    hero_array = filtered_hero;
+    console.log(hero_array);
+    displayHeroes();
+}
+
+async function download_data() {
+    let url = `https://assets.deadlock-api.com/v2/heroes`;
+    let response = await fetch(url); //fetch API
+    hero_array = await response.json(); //convert data from api to JSON format
+
+    hero_array = hero_array.filter((hero) => {
+        return hero.in_development == false //filter in all heroes that are playable
+    })
+
+    console.log(hero_array);
+}
+
+async function initialize_data() {
+    await download_data();
+    document.querySelector("#filterHeroes").value = ""; //clear search results
+    displayHeroes();
+}
+
+
+initialize_data();
